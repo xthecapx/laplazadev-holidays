c6b0a4e2193d88ecd8337fb9e91ec13a5647d21e
xthecapx <xthecapx@gmail.com>
2024-10-03 16:08:22 -0500

    added la flaza waiting approval

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ import chalk from 'chalk';
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const matrixArt = `
-  _                 _____  _                  _____             
- | |               |  __ \\| |                |  __ \\            
- | |     __ _ _ __ | |__) | | __ _ ______ _  | |  | | _____   __
- | |    / _\` | '_ \\|  ___/| |/ _\` |_  / _\` | | |  | |/ _ \\ \\ / /
- | |___| (_| | |_) | |    | | (_| |/ / (_| | | |__| |  __/\\ V / 
- |______\\__,_| .__/|_|    |_|\\__,_/___\\__,_| |_____/ \\___| \\_/  
-             | |                                                
-             |_|                 By Thecap                
+  _           _____  _                _____             
+ | |         |  ___|\| |              |  __ \\            
+ | |     __ _| |___ | | __ _ ___ __ _| |  | | _____   __
+ | |    / _\` |  ___|| |/ _\` |_  / _\` | |  | |/ _ \\ \\ / /
+ | |___| (_| | |    | | (_| |/ / (_| | |__| |  __/\\ V / 
+ |______\\__,_|_|    |_|\\__,_/___\\__,_|_____/ \\___| \\_/  
+                                                        
+                                            By Thecap                     
 `;
 
 async function typeWriter(text, speed = 10) {
@@ -29,7 +29,7 @@ async function getHolidays(country = 'CO', year = '2024') {
     try {
         console.log(chalk.green(matrixArt));
         await typeWriter(chalk.greenBright(`Accessing holiday database for ${country} in ${year}...`));
-        await sleep(500);
+        await sleep(100);
 
         const response = await axios.get(`https://world-holidays.info/api/holidays`, {
             params: { lang: 'en', country, year }
